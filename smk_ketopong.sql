6ddd832f3014b6c3ba63566bd90d9c4af6358ef2
UyizDofukizi <57898357+VirtualRez21@users.noreply.github.com>
2022-10-25 20:29:10 +0800

    Add files via upload

--- smk_ketopong.sql
+++ smk_ketopong.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2022 at 07:30 AM
+-- Generation Time: Oct 25, 2022 at 02:28 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -41,18 +41,15 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`id`, `nisn`, `nama`, `alamat`, `jenis_kelamin`, `foto`) VALUES
-(1, 12345, 'fira', 'samping bengkel', 'perempuan', ''),
-(2, 12346, 'hibatur', 'jalur 2', 'pria', ''),
-(3, 12347, 'davva', 'timbau', 'pria', ''),
-(6, 11111111, 'mardais', 'ketopong', 'pria', ''),
-(8, 9090, 'nisaaaa', 'isekai', 'perempuan', ''),
-(9, 12312, 'Uyiz Dofukizi', 'sdgdf', 'pria', ''),
-(11, 2147483647, 'nisaaaa', 'sdgdf', 'pria', '194938.png'),
+(1, 12345, 'fira', 'samping bengkel', 'perempuan', '2022-10-2508-27-37pmavatar_3.png'),
+(2, 12346, 'hibatur', 'jalur 2', 'pria', '2022-10-2508-26-14pmAvatar-Profile-PNG-Picture.png'),
+(3, 12347, 'davva', 'timbau', 'pria', '2022-10-2508-24-41pm147144.png'),
+(6, 11111111, 'mardais', 'ketopong', 'pria', '2022-10-2508-23-01pm147140.png'),
+(8, 9090, 'nisaaaa', 'isekai', 'perempuan', '2022-10-2508-22-37pm194938.png'),
+(9, 12312, 'Uyiz Dofukizi', 'sdgdf', 'pria', '2022-10-2508-21-05pmavatar_3.png'),
 (12, 2147483647, 'Jeruk Bali', 'sdgdf', 'pria', '194938.png'),
-(13, 12312, 'mardais', 'sdgdf', 'pria', '2022-10-2511:54:36am194938.png'),
 (14, 12312, 'mardais', 'sdgdf', 'pria', '2022-10-2511-54-50am194938.png'),
-(15, 2342, 'asda', 'asdfasd', 'asfa', '2022-10-2512-02-36pm194938.png'),
-(16, 2342, 'sdfs', 'svcs', 'asdfs', '2022-10-2512-03-10pm194938.png');
+(15, 2342, 'asda', 'asdfasd', 'asfa', '2022-10-2512-02-36pm194938.png');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +69,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
